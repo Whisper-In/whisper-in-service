b6098986f52752d9bce257f75ebe0a87443bc658
Edwin Lee <edwinlee107@hotmail.my>
2023-09-29 02:47:50 +0800

    fix deployment error

--- src/database/mongodb.ts
+++ src/database/mongodb.ts
@@ -1,7 +1,5 @@
 import {connect} from "mongoose";
 import { mongoDBConnectionString } from "../config/app.config.js";
 import "../models/user/user-profile.model.js"
-import "../models/ai/ai-profile.model.js"
-
 
 export const connectMongoDB = () => connect(mongoDBConnectionString);
